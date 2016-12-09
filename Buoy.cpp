8b6e19bd4c08102c9c9df66f43ad12eaef1eef35
Dyson <alexandre.galland@ensta-bretagne.org>
2016-12-09 10:06:05 +0100

    Modification des equations d'etat des bouees

--- Buoy.cpp
+++ Buoy.cpp
@@ -8,44 +8,32 @@ extern double dt;
 
 class Buoy 
 {
+    Buoy* b;
     public:
-        double* Lorenz(void);
-        double* Pendulum(void);
-        double* StateEq(void);
+        void Lorenz(void);
+        void Pendulum(void);
+        void StateEq(void);
+        void SetCommand(double ub);
+        int GetNumber(void);
+        double* GetPos(void);
         void Clock(void);
-        Buoy(double xb, double yb, double zb, double ub);
+        Buoy(int nb, double xb, double yb, double zb, double ub);
 
     private:
+        int n;
         double x;
         double y;
         double z;
         double u;
+        double* Xdot;
 };
 
 
 
 
-// test class
-
-
-/*
-class Buoy 
-{
-    public:
-        void setX(double x);
-        double getX(void);
-        Buoy(double xb, double yb, double zb, double ub);
-    private:
-        double x;
-        double y;
-        double z;
-        double u;
-};
-
-*/
-
-Buoy::Buoy(double xb, double yb, double zb, double ub)
+Buoy::Buoy(int nb, double xb, double yb, double zb, double ub)
 {    
+    n = nb;
     x = xb;
     y = yb;
     z = zb;
@@ -53,49 +41,62 @@ Buoy::Buoy(double xb, double yb, double zb, double ub)
 }
 
 
-/*
-Buoy::~Buoy()
-{
-}*/
 
-
-double* Buoy::Lorenz(void)
+void Buoy::Lorenz(void)
 {
     double sigma = 10.0; 
     double beta = 8.0/3.0; 
     double rho = 24.3;
     double k = 1.0;
-    double* Xdot = new double[3];
     Xdot[0] = sigma*(y-x);
     Xdot[1] = x*(rho-z)-y;
     Xdot[2] = k*(x-y-beta*z)+u;
-    return Xdot;
+    
 }
 
 
-double* Buoy::Pendulum(void)
+void Buoy::Pendulum(void)
 {
-    double* Xdot = new double[3];
     Xdot[0] = y;
     Xdot[1] = -sin(x);
     Xdot[2] = u;
-    return Xdot;
 }
 
-double* Buoy::StateEq(void)
+void Buoy::StateEq(void)
 {
-    double* Xdot = new double[3];
     Xdot[0] = sin(0.001*(y+0.9*z));
     Xdot[1] = -sin(0.001*(x+z));
     Xdot[2] = u;
-    return Xdot;
+    
+}
+
+
+void Buoy::SetCommand(double ub)
+{
+    u = ub;
+}
+
+
+
+int Buoy::GetNumber(void)
+{
+    return n;
+}
+
+
+double* Buoy::GetPos(void)
+{
+    double* xd = new double[4];
+    xd[0] = sqrt(pow(Xdot[0],2.0)+pow(Xdot[1],2.0)+pow(Xdot[2],2.0));
+    xd[1] = x;
+    xd[2] = y;
+    xd[3] = z;
+    return xd;
 }
 
 void Buoy::Clock(void)  // The model is described in "L. Jaulin Modélisation et commande d'un bateau à voile, CIFA2004, Douz (Tunisie)"
 {   
-    Buoy b(0.0,0.0,0.0, 0.0);
-    double* Xdot = new double[3];
-    Xdot = b.Lorenz();
+    Lorenz();
     x = x+dt*Xdot[0];
     y = y+dt*Xdot[1];
     z = z+dt*Xdot[2];
--- Buoy.h
+++ Buoy.h
@@ -10,11 +10,18 @@ class Buoy
             double beta,sigma,rho,k; //parameters
             double xdot,ydot,zdot;  //link variables
 
-            void Clock();
-            void Pendulum();
-            void Lorenz();
-            explicit Buoy ();
-        ~Buoy();
+
+            void Lorenz(void);
+            void Pendulum(void);
+            void StateEq(void);
+            void SetCommand(double ub);
+            int GetNumber(void);
+            double* GetPos(void);
+            void Clock(void);
+            Buoy(int nb, double xb, double yb, double zb, double ub);
+            
+            //explicit Buoy ();
+        //~Buoy();
  };
 
 #endif // Buoy_H
