21b89cb749e1f81696a834953d0b780e7c0eb8f7
pierrebenet <pierrebenet96@gmail.com>
2016-12-09 10:55:46 +0100

    les bouees s'affichent et sont initialisée sous la mer

--- Buoy.h
+++ Buoy.h
@@ -10,11 +10,18 @@ class Buoy
             double beta,sigma,rho,k; //parameters
             double xdot,ydot,zdot;  //link variables
 
-            void Clock();
-            void Pendulum();
-            void Lorenz();
-            explicit Buoy ();
-        ~Buoy();
+
+            void Lorenz(void);
+            void Pendulum(void);
+            void StateEq(void);
+            void SetCommand(double ub);
+            int GetNumber(void);
+            double* GetPos(void);
+            void Clock(void);
+            Buoy(int nb, double xb, double yb, double zb, double ub);
+            
+            inline Buoy (){x=0;y=0;z=-10;xdot=0;ydot=0;zdot=0;}
+        //~Buoy();
  };
 
 #endif // Buoy_H
